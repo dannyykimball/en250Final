48ebb5f2a7d3e6e425264c919dfd01855249bee2
Daniel Kimball <dannyykimball@gmail.com>
2021-05-10 21:43:31 -0400

    pizzaz

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import "./styles.css";
+import Particles from "react-particles-js";
 
 function App() {
   return (
     <div>
       <header>
         EN 250 Final
+        <div className="part">
+          <Particles
+            params={{
+              particles: {
+                number: {
+                  value: 50,
+                },
+                size: {
+                  value: 3,
+                },
+              },
+              interactivity: {
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: "repulse",
+                  },
+                },
+              },
+            }}
+          />
+        </div>
         <div>Energy Flow Modeling</div>
         <div className="subtitleRow">Miles Rosenberg & Daniel Kimball</div>
       </header>
