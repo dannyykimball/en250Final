6d329fef735c14b84dd1b96cf42c9b13251f45cb
Daniel Kimball <dannyykimball@gmail.com>
2021-05-10 21:32:56 -0400

    title template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        EN 250 Final
+        <div>Energy Flow Modeling</div>
+        <div className="subtitleRow">Miles Rosenberg & Daniel Kimball</div>
       </header>
+      <div>Overview</div>
+      <div>Derivation</div>
+      <div>Applications</div>
+      <div>Simulation</div>
+      <div>Works Cited</div>
     </div>
   );
 }
