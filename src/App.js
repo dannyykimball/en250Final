ddfe360ba98cd05f64780de20ead4d7b02f95495
Daniel Kimball <dannyykimball@gmail.com>
2021-05-10 22:13:54 -0400

    layout template

--- src/App.js
+++ src/App.js
@@ -1,41 +1,57 @@
 import "./styles.css";
 import Particles from "react-particles-js";
+import banner from "./banner.png";
 
 function App() {
   return (
     <div>
-      <header>
-        EN 250 Final
-        <div className="part">
-          <Particles
-            params={{
-              particles: {
-                number: {
-                  value: 50,
-                },
-                size: {
-                  value: 3,
+      <div className="part">
+        <Particles
+          params={{
+            particles: {
+              number: {
+                value: 50,
+              },
+              size: {
+                value: 3,
+              },
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "repulse",
                 },
               },
-              interactivity: {
-                events: {
-                  onhover: {
-                    enable: true,
-                    mode: "repulse",
-                  },
-                },
-              },
-            }}
-          />
-        </div>
-        <div>Energy Flow Modeling</div>
-        <div className="subtitleRow">Miles Rosenberg & Daniel Kimball</div>
-      </header>
-      <div>Overview</div>
-      <div>Derivation</div>
-      <div>Applications</div>
-      <div>Simulation</div>
-      <div>Works Cited</div>
+            },
+          }}
+        />
+      </div>
+      <div>
+        <header>
+          <img className="banner" src={banner} alt="banner" />
+          <div>
+            EN 250 Final <br />
+            Energy Flow Modeling
+          </div>
+          <div className="subtitleRow">Miles Rosenberg & Daniel Kimball</div>
+        </header>
+
+        <div className="subsubtitleRow">Overview</div>
+        <div className="content">this is content</div>
+
+        <div className="subsubtitleRow">Derivation</div>
+        <div className="content">this is content</div>
+
+        <div className="subsubtitleRow">Applications</div>
+        <div className="content">this is content</div>
+
+        <div className="subsubtitleRow">Simulation</div>
+        <div className="content">this is content</div>
+
+        <div className="subsubtitleRow">Works Cited</div>
+        <div className="content">this is content</div>
+      </div>
     </div>
   );
 }
