bf65a47611cc88196a4eb2124e626c43ee921929
Daniel Kimball <dannyykimball@gmail.com>
2021-05-10 22:52:46 -0400

    added routes

--- src/simulation.js
+++ src/simulation.js
@@ -1,18 +1,36 @@
-import "./styles.css";
-import banner from "./img/banner.png";
+import "./sim.css";
+import hawkimg from "./img/hawk.jpg";
+import snakeimg from "./img/snake.jpg";
+import bunnyimg from "./img/bunny.jpg";
+import grassimg from "./img/grass.jpg";
+import { useState, useEffect } from "react";
 
 export default function Sim() {
+  const [grass, setGrass] = useState();
+  useEffect(() => {
+    setGrass(100);
+  }, []);
+
   return (
     <div>
       <header>
-        <img className="banner" src={banner} alt="banner" />
-        <div>
-          EN 250 Final <br />
-          Energy Flow Modeling
-        </div>
+        <div>Energy Flow Simulation</div>
         <div className="subtitleRow">Miles Rosenberg & Daniel Kimball</div>
       </header>
-      <div>sim</div>
+      <div className="simmmer">
+        <div className="animal">
+          <img className="animalimg" src={hawkimg} alt="hawk" />
+        </div>
+        <div className="animal">
+          <img className="animalimg" src={snakeimg} alt="snake" />
+        </div>
+        <div className="animal">
+          <img className="animalimg" src={bunnyimg} alt="bunny" />
+        </div>
+        <div className="animal">
+          <img className="animalimg" src={grassimg} alt="grass" />
+        </div>
+      </div>
     </div>
   );
 }
